e8c893e188cdb14d878fb93797f0807025ccc64b
Montilla Francisco <fj.montilla@soprasteria.com>
2023-01-23 15:16:38 +0100

    C4U: Implementation of Spring security and JWT token validation based on permissions

--- src/main/java/com/cook4u/controller/UserController.java
+++ src/main/java/com/cook4u/controller/UserController.java
@@ -2,12 +2,11 @@ package com.cook4u.controller;
 
 import com.cook4u.model.user.UserEntity;
 import com.cook4u.service.impl.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
 
     private UserService userService;
@@ -17,11 +16,13 @@ public class UserController {
     }
 
     @GetMapping("/users")
+    @PreAuthorize("hasAuthority('crud:user')")
     public Iterable<UserEntity> getAllUsers() {
         return this.userService.findAll();
     }
 
     @PostMapping("/users")
+    @PreAuthorize("hasAuthority('crud:user')")
     public UserEntity create(@RequestBody UserEntity user) {
         return userService.create(user);
     }
